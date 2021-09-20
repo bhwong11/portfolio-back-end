a411aab0a0936059cb80234946cad7741c250e61
bhwong11 <user@email.com>
2021-09-19 21:31:18 -0400

    sending json on server

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+//external modules
 const express = require('express');
 const app = express();
+const cors = require('cors')
 PORT = 4000
 
+//middleware
+app.use(cors())
 
 app.get('/',async (req,res)=>{
     res.send('hello world')
+})
+
+app.get('/jsontest',async (req,res)=>{
+    try{
+        res.status(200).json({
+            "test":"test value",
+        })
+    }
+    catch(error){
+        console.log(error)
+        res.send('error occured:',error)
+    }
+    
 })
 
 app.listen(PORT,function(){

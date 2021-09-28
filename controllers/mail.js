c6b2ed78942a5b329b7e9b40db5120c9c2c24ae5
bhwong11 <user@email.com>
2021-09-28 12:19:03 -0400

    added mail route and controler

--- controllers/mail.js
+++ controllers/mail.js
@@ -1,3 +1,5 @@
+//dotenv import
+require('dotenv').config();
 
 //node mailer import
 const nodemailer = require('nodemailer');
@@ -6,20 +8,23 @@ const nodemailer = require('nodemailer');
 const send = async(req,res)=>{
     try{
 
+        const email = req.body.email
+        const subject = req.body.subject
+        const content = req.body.content
 
         const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'bryanthwong1@gmail.com',
-            pass: 'Test1234Pokemon'
+            pass: process.env.EMAILPASSWORD
         }
         });
 
         const mailOptions = {
         from: 'bryanthwong1@gmail.com',
         to: 'bryanthwong@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
+        subject: `${subject}`,
+        text: `Email:${email} message: ${content}`
         };
 
         transporter.sendMail(mailOptions, function(error, info){
@@ -33,7 +38,7 @@ const send = async(req,res)=>{
             console.log('Email sent: ' + info.response);
             return res.status(200).json({
                 status:200,
-                message:"sucessful mail send"
+                message:"sucessful mail sent"
             })
         }
         });
